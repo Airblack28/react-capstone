2027a1b4d1c6fa83bb89f10504cc141369d325aa
Airblack28 <ikandoarthur@gmail.com>
2023-11-07 03:51:04 +0100

    Project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,52 @@ import Header from './Header.js';
 import Main from './Main';
 import Nav from './Nav';
 import Menu from './Menu';
-import Booking from './Booking';
+import BookingForm from './BookingForm.js';
+import React, { useReducer } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
+
+  const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
+const fetchAPI = function(date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for(let i = 17; i <= 23; i++) {
+        if(random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if(random() < 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+const submitAPI = function(formData) {
+    return true;
+};
+
+const initialState = {availableTimes:  fetchAPI(new Date())}
+const [state, dispatch] = useReducer(updateTimes, initialState);
+
+function updateTimes(state, date) {
+    return {availableTimes: fetchAPI(new Date(date))}
+}
+
+const navigate = useNavigate();
+function submitForm (formData) {
+    if (submitAPI(formData)) {
+        navigate("/confirmed")
+    }
+}
 
   return (
     <>
@@ -14,6 +57,7 @@ function App() {
       {/* <Header/> */}
       <Main></Main>
       <Menu></Menu>
+      <BookingForm availableTimes={state} dispatch={dispatch} submitForm={submitForm}></BookingForm>
       <Footer></Footer>
     </>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,26 @@
+import React, { useState } from "react";
 import './App.css';
 import logo from './Logo.svg'
 
 function Nav() {
-  return (
-    <nav>
-      <a href='/'>
-        <img src={logo} alt='logo'/>
-      </a>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      <ul>
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+      <a href="/" className="logo">
+        <img src={logo} alt="" />
+      </a>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
         <li><a href='/'>Home</a></li>
         <li><a href='/'>About</a></li>
         <li><a href='/'>Menu</a></li>
